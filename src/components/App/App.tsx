85153221a476960da649d4b1e922d6251293fa71
Grumaks <grumaks@ya.ru>
2021-04-25 22:01:24 +0300

    настройка сборки

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import "./App.scss";
+
+class App extends Component {
+    render(): JSX.Element {
+        return (
+            <div>
+                <h1>Hello, World</h1>
+            </div>
+        );
+    }
+}
+export default App;
