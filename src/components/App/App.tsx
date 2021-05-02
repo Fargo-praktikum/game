37c5300ab3f5d29019cddda3d48c6dff6807463f
Grumaks <grumaks@ya.ru>
2021-05-02 19:11:19 +0300

    обработка ошибок в детях

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,38 +9,42 @@ import { GamePage } from "../pages/GamePage";
 
 import "./App.scss";
 import "../../assets/vendor/normalize.scss";
+import { ErrorBoundary } from "../ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
     render(): JSX.Element {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/" exact>
-                        <div>main</div>
-                    </Route>
-                    <Route path="/login">
-                        <div>login</div>
-                    </Route>
-                    <Route path="/signup">
-                        <SignupPage />
-                    </Route>
-                    <PrivateRoute path="/profile">
-                        <ProfilePage />
-                    </PrivateRoute>
-                    <PrivateRoute path="/leaderboard">
-                        <LeaderboardPage />
-                    </PrivateRoute>
-                    <PrivateRoute path="/forum">
-                        <div>forum</div>
-                    </PrivateRoute>
-                    <PrivateRoute path="/game">
-                        <GamePage />
-                    </PrivateRoute>
-                    <Route path="*">
-                        <NotFoundPage />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
+                <BrowserRouter>
+                    <Switch>
+                        <ErrorBoundary>
+                            <Route path="/" exact>
+                                <div>main</div>
+                            </Route>
+                            <Route path="/login">
+                                <div>login</div>
+                            </Route>
+                            <Route path="/signup">
+                                <SignupPage />
+                            </Route>
+                            <PrivateRoute path="/profile">
+                                <ProfilePage />
+                            </PrivateRoute>
+                            <PrivateRoute path="/leaderboard">
+                                <LeaderboardPage />
+                            </PrivateRoute>
+                            <PrivateRoute path="/forum">
+                                <div>forum</div>
+                            </PrivateRoute>
+                            <PrivateRoute path="/game">
+                                <GamePage />
+                            </PrivateRoute>
+                        </ErrorBoundary>
+                        <Route path="*">
+                            <NotFoundPage />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+
         );
     }
 }
