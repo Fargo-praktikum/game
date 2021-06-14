aeb07e2dc6911d6c2b43d207607c9a22d9d2ec08
Grumaks <grumaks@ya.ru>
2021-06-15 00:52:09 +0300

    fix

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 const { app } = require("./serverDist/server.js");
-const https = require("https");
-const selfSigned = require("openssl-self-signed-certificate");
 
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5000;
 const isDev = process.env.NODE_ENV === "development";
 
-if (isDev) {
-    https
-        .createServer({key: selfSigned.key, cert: selfSigned.cert}, app)
-        .listen(port, '0.0.0.0', () => {
-            console.info(`https://localhost:${port}`);
-        });
-}
-else {
-    app.listen(port, () => {
-        console.log("Application is started on localhost:", port);
-    });
-}
+app.listen(port, () => {
+    console.log("Application is started on localhost:", port);
+});
