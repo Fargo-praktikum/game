720ece80d9e5835f0214637f7afb2a8235433ba7
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2021-07-02 14:43:01 +0300

    game navbar

--- src/components/pages/GamePage/GamePage.tsx
+++ src/components/pages/GamePage/GamePage.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Game } from "../../Game";
+import Navbar from "../../Navbar/Navbar";
+import "./gamePage.scss";
 
 export const GamePage = (): JSX.Element => {
-    return <Game />;
+    return (
+        <div className="game__wrap">
+            <Navbar />
+            <div className="game__field">
+                <Game />
+            </div>
+        </div>
+    );
 };
