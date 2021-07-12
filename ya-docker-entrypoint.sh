57464a0e298969a5781628c55ce971dffe965106
Grumaks <grumaks@ya.ru>
2021-07-12 03:19:58 +0300

    .

--- ya-docker-entrypoint.sh
+++ ya-docker-entrypoint.sh
@@ -1,3 +1,4 @@
 echo "migrate"
 npx sequelize db:migrate --migrations-path /app/migrations --url postgresql://postgres:newPassword@postgres:5432/fargo-cards
 node /app/serverDist/server.js
+echo "end"
